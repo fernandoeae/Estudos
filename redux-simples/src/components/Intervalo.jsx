e3a241bd3a049de05130dd6650210f6ef6bb20c8
fernandoeae <89745492+fernandoeae@users.noreply.github.com>
2021-08-30 14:50:59 -0300

    Terminano conceitos Redux

--- redux-simples/src/components/Intervalo.jsx
+++ redux-simples/src/components/Intervalo.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import Card from './Card'
 import './Intervalo.css'
 
-export default props => {
+import { connect } from 'react-redux'
+
+function Intervalo(props){
+    const {min, max} = props
     return (
         <Card title="Intevalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={0} />
+                    <input type="number" value={min} readOnly />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={10} />
+                    <input type="number" value={max } readOnly />
                 </span>
             </div>
         </Card>
     )
 }
+
+
+function mapStateToProps(state){
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Intervalo)
