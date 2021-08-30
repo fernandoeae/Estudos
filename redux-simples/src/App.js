a6202d812c9e706a0072c212201660791d423240
fernandoeae <89745492+fernandoeae@users.noreply.github.com>
2021-08-30 10:28:37 -0300

    Implementando componentes

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,20 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card'
+import React, { useState } from 'react'
+
+import Intervalo from './components/Intervalo'
+import Soma from './components/Soma'
+import Media from './components/Media'
+import Sorteio from './components/Sorteio'
 
 function App() {
+    
+    const [min, setMin] = useState(1)
+    const [max, setMax] = useState(10) 
+
     return ( 
       <div className = "App" >
         <h1>Exercicio React (Redux-Simples)</h1>
 
         <div className="linha" >
-          <Card title="Card 1" red>X</Card>
-          <Card title="Card 2" green>X</Card>
+          <Intervalo ></Intervalo>
         </div>
 
         <div className="linha">
-          <Card title="Card 3" blue>Y</Card>
-          <Card title="Card 4" purple>Y</Card>
+          <Media min={min} max={max} ></Media>
+          <Soma  min={min} max={max}></Soma>
+          <Sorteio min={min} max={max}></Sorteio>
         </div>
 
       </div>
